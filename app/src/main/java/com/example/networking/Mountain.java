853085b44f250ff48552110a2efafd8e46120771
Claes Svensson <a19clasv@student.his.se>
2022-04-27 23:55:35 +0200

    Added Mountain class

---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,30 @@
+package com.example.networking;
+
+public class Mountain {
+
+    String ID;
+    String name;
+    String location;
+    int size;
+    int cost;
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+}
