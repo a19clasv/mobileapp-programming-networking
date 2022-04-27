6091142e5e409648e7820c45f8ac085f629e5ec3
Claes Svensson <a19clasv@student.his.se>
2022-04-28 00:45:11 +0200

    Added RecyclerView.Adapter MyAdapter

---
+++ app/src/main/java/com/example/networking/MyAdapter.java
@@ -0,0 +1,38 @@
+package com.example.networking;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+    private ArrayList<Mountain> mountains;
+
+    public  MyAdapter(ArrayList<Mountain> mountains){
+        this.mountains = mountains;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_mountain, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.title.setText(mountains.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mountains.size();
+    }
+}
