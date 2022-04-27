cf5426d55f0ae94c7b133af3ae679a9cd352c57a
Claes Svensson <a19clasv@student.his.se>
2022-04-28 00:28:18 +0200

    Added RecyclerView.ViewHolder MyViewHolder

---
+++ app/src/main/java/com/example/networking/MyViewHolder.java
@@ -0,0 +1,17 @@
+package com.example.networking;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyViewHolder extends RecyclerView.ViewHolder {
+    public TextView title;
+
+    public MyViewHolder(@NonNull View itemView) {
+        super(itemView);
+        title = itemView.findViewById(R.id.title);
+
+    }
+}
