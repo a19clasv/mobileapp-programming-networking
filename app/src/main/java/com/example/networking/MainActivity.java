38066cd885500a99581f79d92c8d3f30d0188a5c
Claes Svensson <a19clasv@student.his.se>
2022-04-28 01:29:27 +0200

    Displays the names of the mountains loaded from a URL in the RecyclerView

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -7,28 +7,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
-    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
-    private final String JSON_FILE = "mountains.json";
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
 
     private ArrayList<Mountain> mountains;
     private RecyclerView.Adapter adapter;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new JsonFile(this, this).execute(JSON_FILE);
+        recyclerView = findViewById(R.id.recycler_view);
+
+
+        new JsonTask(this).execute(JSON_URL);
     }
 
     @Override
     public void onPostExecute(String json) {
-        Log.d("MainActivity", json);
+        // Create GSON object to perform marshall/unmarshall operations
+        Gson gson = new Gson();
+
+// Unmarshall JSON -> list of objects
+        Type type = new TypeToken<ArrayList<Mountain>>() {}.getType();
+        mountains = gson.fromJson(json, type);
+        adapter = new MyAdapter(mountains);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
 }
